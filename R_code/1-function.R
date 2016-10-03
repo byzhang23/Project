7179ecb8355ef9717d4c08f7eec3489e2303e6f5
byzhang23 <byzhang23@gmail.com>
2016-10-03 11:12:43 -0400

    update 1-function.R

--- R_code/1-function.R
+++ R_code/1-function.R
@@ -1,8 +1,9 @@
-#extract data of MD(24) and DC(11)
+#extract data of MD(24) and DC(11) and add geo_id
 
 filter_dat <- function(file_path){
     tmp=read_sas(file_path)
     colnames(tmp)=tolower(colnames(tmp))
     tmp=tmp %>% filter(state == 24 | state == 11) 
+    tmp$geo_id=with(tmp,as.integer(paste0(state,formatC(county, width=3, flag="0"))))
     return (tmp)
 }
